e0016e001a4e6f02768c6a780a1ef4a6fb6e5a6f
Bill Cortright <BCort28@gmail.com>
2021-01-19 06:16:19 -0500

    File Upload

--- app.js
+++ app.js
@@ -3,13 +3,14 @@ var express = require("express");
 var path = require("path");
 var logger = require("morgan");
 const passport = require("passport");
-const config = require('./config');
+const config = require("./config");
 
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
 const campsiteRouter = require("./routes/campsiteRouter");
 const promotionRouter = require("./routes/promotionRouter");
 const partnerRouter = require("./routes/partnerRouter");
+const uploadRouter = require("./routes/uploadRouter");
 
 const mongoose = require("mongoose");
 
@@ -28,16 +29,16 @@ connect.then(
 
 var app = express();
 
-app.all('*', (req, res, next) => {
+app.all("*", (req, res, next) => {
   if (req.secure) {
     return next();
   } else {
     console.log(
-      `Redirecting to: https://${req.hostname}:${app.get('secPort')}${req.url}`
+      `Redirecting to: https://${req.hostname}:${app.get("secPort")}${req.url}`
     );
     res.redirect(
       301,
-      `https://${req.hostname}:${app.get('secPort')}${req.url}`
+      `https://${req.hostname}:${app.get("secPort")}${req.url}`
     );
   }
 });
@@ -61,7 +62,7 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use("/campsites", campsiteRouter);
 app.use("/promotions", promotionRouter);
 app.use("/partners", partnerRouter);
-
+app.use("/imageUpload", uploadRouter);
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
   next(createError(404));
